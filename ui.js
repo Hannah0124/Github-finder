5c4e7727346db7c6624a62d29146f4719a82db88
Hannah0124 <hannahjoo24@gmail.com>
2019-10-24 01:27:18 -0700

    Fetch & Display Repos

--- ui.js
+++ ui.js
@@ -13,10 +13,10 @@ class UI {
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success">Followers: ${user.followers}</span>
-            <span class="badge badge-info">Following: ${user.following}</span>
+            <span class="badge badge-primary mb-1">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary mb-1">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success mb-1">Followers: ${user.followers}</span>
+            <span class="badge badge-info mb-1">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group">
               <li class="list-group-item">Company: ${user.company}</li>
@@ -31,6 +31,32 @@ class UI {
       <div id="repos"></div>
     `;
   }
+
+  // Show user repos 
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_balnk">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos 
+    document.getElementById('repos').innerHTML = output;
+  }
+  
 
   // Show alert message
   showAlert(message, className) {
